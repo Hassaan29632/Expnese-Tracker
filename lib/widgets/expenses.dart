0537716b9276e1653cbfc6ccd2b140b2c4e5fd1f
hassaan <hassaanahhmed@gmail.com>
2025-08-13 14:58:13 +0500

    adding showmodelbottomsheet in plus button on app bar

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -14,26 +14,38 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registeredExpenses = [
     Expense(
-        date: DateTime.now(),
-        title: "flutter course ",
-        amount: 99,
-        category: Category.work ),
+      date: DateTime.now(),
+      title: "flutter course ",
+      amount: 99,
+      category: Category.work,
+    ),
 
     Expense(
-        date: DateTime.now(),
-        title: "cenima ",
-        amount: 99,
-        category: Category.leisure )
+      date: DateTime.now(),
+      title: "cenima ",
+      amount: 99,
+      category: Category.leisure,
+    ),
   ];
 
+  void _openAddExpenseOvelay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) =>  Text("enter data here"),);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOvelay, icon: Icon(Icons.add)),
+        ],
+      ),
       body: Column(
         children: [
           Text("the chart"),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses,))
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
